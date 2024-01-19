2b99e1356975336dfb56d4e89f7cf4d13df634e2
Humayra-R <humayra@Humayra>
2024-01-19 13:01:54 +0000

    12-get-api-articles-article_id complete

--- models/article-model.js
+++ models/article-model.js
@@ -20,8 +20,7 @@ function responseAllArticles(filter = null) {
 }
 
 function responseArticle(article_id) {
-    return db.query(`SELECT * FROM articles
-    WHERE article_id = $1`, [article_id])
+    return db.query(`SELECT articles.author, title, articles.article_id, topic, articles.body, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.author, title, articles.article_id, topic, articles.body, articles.created_at, articles.votes, article_img_url ORDER BY articles.created_at DESC`, [article_id])
     .then(({ rows }) => {
         if (rows.length === 0) {
             return Promise.reject({ status:404, msg: 'article does not exist' })
